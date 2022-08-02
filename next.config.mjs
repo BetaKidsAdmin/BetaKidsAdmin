16066e2d91f7d5fe58a4d4347b8663945e46ba43
necheporenko <ne4eporenko.v@gmail.com>
2022-08-02 19:02:25 +0300

    fix: disable eslint errors on build

--- next.config.mjs
+++ next.config.mjs
@@ -142,6 +142,11 @@ const config = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 };
 
